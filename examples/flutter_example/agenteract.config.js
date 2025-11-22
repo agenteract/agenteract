e4410e9196dab772039c4674fa1a3fd321488338
Mike Ribbons <mribbons@gmail.com>
2025-11-22 15:28:48 +1100

    feat: Add deep link scheme to flutter example
    + remove some incorrectly tracked artifacts

---
+++ examples/flutter_example/agenteract.config.js
@@ -0,0 +1,13 @@
+export default {
+  "port": 8766,
+  "projects": [
+    {
+      "name": "flutter-example",
+      "path": ".",
+      "devServer": {
+        "command": "flutter run",
+        "port": 8793
+      }
+    }
+  ]
+};
